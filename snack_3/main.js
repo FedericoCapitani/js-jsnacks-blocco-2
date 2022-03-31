86f9c2a421dd01e59229fb57d47bd9c1e82bad11
FedericoCapitani <federico.capitani@outlook.it>
2022-03-31 15:12:09 +0200

    snack 3

---
+++ snack_3/main.js
@@ -0,0 +1,17 @@
+/* SNACK 3
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome. */
+
+const nomi = ['Mario ', 'Sara ', 'Luca ', 'Laura ', 'Marco ']
+const cognomi = ['Rossi', 'Manca', 'Asuni', 'Piras', 'Vacca']
+let lista_falsa = []
+
+const numero_lista_falsa = parseInt(prompt('Quanti invitati deve avere la lista?'))
+
+for (let i = 0; i < numero_lista_falsa; i++){
+    let a = Math.floor(Math.random() * 4)
+    let b = Math.floor(Math.random() * 4)
+    let element_lista_falsa = nomi[a] + cognomi[b]
+    lista_falsa[i] = element_lista_falsa
+}
+
+console.log(lista_falsa)
